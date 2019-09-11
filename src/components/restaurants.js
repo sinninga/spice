ae2d240beeb50605cb29373151eaf8bbb36545d6
Adam Sinning <asinning25@gmail.com>
2019-09-11 15:00:55 -0600

    headers working

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,23 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function Restaurants() {
+const yelp_key='FPGJ2LPgJpxCoAN1CnIaBMNMqHRUjRXsHFdRjd85XtPcR_cW3iIaC6JVcYmBu7pVcjQBgcusX-pB-oAX7zR1WhR1kXbdGQ3hyOIieEQDJ0MasdBHUXvMxq8Q-KN2XXYx';
+// const url = 'GET https://api.yelp.com/v3/businesses/search';
 
-  useEffect(() => {
-    fetchItems();
-  }, []);
 
-  const fetchItems = async () => {
-    const data = await fetch('GET https://api.yelp.com/v3/businesses/search?term=spicy&location=colorado&limit=5');
-    
-    const items = await data.json();
-    console.log(items);
+const config = {
+  headers: {
+    "accept": "application/json",
+    "x-requested-with": "xmlhttprequest",
+    "Access-Control-Allow-Origin":"*",
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers':  'Authorization',
+    'Authorization': `Bearer ${yelp_key}`,
+},
+  params: {
+    term: 'spicy',
+    location: 'colorado',
+    limit: 5
   }
+};
 
-  return (
-    <div className="restaurants">
-      Restaurants Page
-    </div>
-  );
+class Restaurants extends Component {
+
+  componentDidMount() {
+    console.log(config);
+    axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search', config)
+    .then(response => console.log(response));
+    }
+
+  render() {
+    return (
+      <div className="restaurants">
+        Restaurants Page
+      </div>
+    )
+  };
 }
 
 export default Restaurants;
