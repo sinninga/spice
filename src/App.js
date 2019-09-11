1508cb30a30e44b390950d782fac118a120d1d44
Adam Sinning <asinning25@gmail.com>
2019-09-11 12:26:22 -0600

    react-router added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Navbar from './components/navbar';
 import Info from './components/info';
 import Recipes from './components/recipes';
 import Restaurants from './components/restaurants';
-import 'react-dom';
-import { BrowserRouter as Router, Route } from "react-dom";
+import 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Restaurants />
-      <Recipes />
-      <Info />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/" component={Restaurants} />
+        <Route path="/recipes" component={Recipes} />
+        <Route path="/info" component={Info} />
+      </div>
+    </Router>
+      
   );
 }
 
