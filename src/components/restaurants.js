e7a1bf264d6ab97aafa84f71b8c9e54454e868a3
Adam Sinning <asinning25@gmail.com>
2019-09-15 13:43:29 -0600

    leaflet map added

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Item from './item';
+import L from 'leaflet';
 
 const yelp_key='FPGJ2LPgJpxCoAN1CnIaBMNMqHRUjRXsHFdRjd85XtPcR_cW3iIaC6JVcYmBu7pVcjQBgcusX-pB-oAX7zR1WhR1kXbdGQ3hyOIieEQDJ0MasdBHUXvMxq8Q-KN2XXYx';
 // const url = 'GET https://api.yelp.com/v3/businesses/search';
@@ -45,6 +46,7 @@ class Restaurants extends Component {
     
     async componentDidMount() {
     // console.log(config);
+    
     const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${this.state.location}&term=${this.state.term}&limit=${this.state.limit}`;
     const response = await fetch(url, config);
     const data = await response.json();
@@ -57,6 +59,12 @@ class Restaurants extends Component {
       lat.push(data.businesses[x].coordinates.latitude)
     }
     // .then(response => console.log(response.data.businesses[0].name))
+    var mymap = L.map('mapid').setView([lat[0], long[0]], 13);
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(mymap);
+    L.marker([lat[0], long[0]]).addTo(mymap);
     // .then(response => console.log(response.data.businesses));
     // const results = response.data
     // console.log(data.businesses);
@@ -110,7 +118,7 @@ class Restaurants extends Component {
               )}
             </div>
         </div>
-        <div className="test"><h1>{console.log(this.state.lat[0])}</h1></div>
+        <div id="mapid"></div>
       </div>
     )
   };
