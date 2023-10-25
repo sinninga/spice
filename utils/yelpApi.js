8abf2850f6c1b2ce5382233c43e42f427c6bbdde
Adam Sinning <asinning25@gmail.com>
2023-10-25 16:47:09 -0600

    retry

--- utils/yelpApi.js
+++ utils/yelpApi.js
@@ -1,18 +1,22 @@
-import axios from 'axios';
-
 const fetchYelpData = async (location, term, limit, apiKey) => {
-  const apiUrl = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&limit=${limit}`;
-  const headers = {
-    Authorization: `Bearer ${apiKey}`,
+    const apiUrl = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&limit=${limit}`;
+    const headers = new Headers({
+      'Authorization': `Bearer ${apiKey}`,
+    });
+  
+    try {
+      const response = await fetch(apiUrl, { method: 'GET', headers: headers });
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data from Yelp API. Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to fetch data from Yelp API');
+    }
   };
-
-  try {
-    const response = await axios.get(apiUrl, { headers });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Failed to fetch data from Yelp API');
-  }
-};
-
-export default fetchYelpData;
+  
+  export default fetchYelpData;  
