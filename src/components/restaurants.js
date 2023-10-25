f3a4e787e7eb5137ecca32947005fd770f9a152b
Adam Sinning <asinning25@gmail.com>
2023-10-25 16:30:21 -0600

    comment

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -80,6 +80,7 @@ class Restaurants extends Component {
     // .then(response => console.log(response.data.businesses));
     // const results = response.data
     // console.log(data.businesses);
+    // console.log();
   }
   
   handleInputChange = (event) => {
