c8ccabb90fdffb42ce9cad9aaded397bd16e9636
Adam Sinning <asinning25@gmail.com>
2023-10-25 17:05:07 -0600

    error handling

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -52,8 +52,12 @@ class Restaurants extends Component {
     async componentDidMount() {
     // console.log(config);
     
-    const response = await axios.get(`${API_URL}?location=${this.state.location}&term=${this.state.term}&limit=${this.state.limit}`);
+    const response = await fetch(`${API_URL}?location=${this.state.location}&term=${this.state.term}&limit=${this.state.limit}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from Yelp API. Status: ${response.status}`);
+    }
     const data = await response.json();
+
     this.setState({ restaurants: data.businesses });
     // console.log(data.businesses);
     for(var i in data.businesses) {
@@ -85,10 +89,15 @@ class Restaurants extends Component {
   
   handleInputChange = (event) => {
     // event.preventDefault()
-    axios.get(`${API_URL}?location=${event.target.value}&term=${this.state.term}&limit=${this.state.limit}`)
-    .then((response) => {
-      // setTimeout(() => {
-        console.log(response.data.businesses);
+    fetch(`${API_URL}?location=${event.target.value}&term=${this.state.term}&limit=${this.state.limit}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data from Yelp API. Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.businesses);
         if(response.data.businesses.length === 0) {
           mymap.remove();
           lat = 0;
